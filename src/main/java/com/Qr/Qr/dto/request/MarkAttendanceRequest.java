77d0e74e2e02f8ca0c26bc7c0ca670493836c85a
Wizard King <116667033+Denxike@users.noreply.github.com>
2026-03-16 12:41:46 +0300

    Change studentId type to Long in MarkAttendanceRequest
    Changed studentId type from String to Long for better data handling.

--- src/main/java/com/Qr/Qr/dto/request/MarkAttendanceRequest.java
+++ src/main/java/com/Qr/Qr/dto/request/MarkAttendanceRequest.java
@@ -15,7 +15,7 @@ import java.util.Date;
 public class MarkAttendanceRequest {
     @NotBlank(message = "StudentId is required")
     @Pattern(regexp = "^[A-Z]{1,3}\\d{2}/\\d{5}/\\d{2}")
-    private String studentId;
+    private Long studentId;
     private String qrToken;
     private String qrSessionId;
     private LocalDateTime markedAt;
